9a4435266c2bc958f8ee82f71b19ed1df9f238ab
Mateusz Sobierajski <mateusz.sobierajski.official@gmail.com>
2023-11-17 16:57:43 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins="*") #DEV ONLY!
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    print("Function triggered!")
+    content = request.json
+    print(content['ID'])
+    if content['ID'] == '1234':
+        return jsonify({"status": "success", "message": "Login successful!"})
+    return jsonify({"status": "failure", "message": "Device of this ID doesn't exist!"})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+#flask run --host=0.0.0.0 --port=5000
