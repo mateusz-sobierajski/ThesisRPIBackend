b815d1c5ded1a26846d2e5e856cc7edbf6d7069e
Mateusz Sobierajski <mateusz.sobierajski.official@gmail.com>
2023-11-18 17:08:41 +0100

    Pumps communication

--- app.py
+++ app.py
@@ -20,7 +20,22 @@ def login():
     return jsonify({"status": "failure", "message": "Device of this ID doesn't exist!"})
 
 
+@app.route('/pumps', methods=['POST'])
+def pumps():
+    print("Function triggered!")
+    content = request.json
+    print(content['ID'])
+    print(content['value'])
+    if content['ID'] == '1':
+        return jsonify({"status": "success", "message": "Pump1 control successful!"})
+    elif content['ID'] == '2':
+        return jsonify({"status": "success", "message": "Pump2 control successful!"})
+    else:
+        return jsonify({"status": "failure", "message": "Device of this ID doesn't exist!"})
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 #flask run --host=0.0.0.0 --port=5000
