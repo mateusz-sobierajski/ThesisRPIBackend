5444e46ad37e7821d108897a95c844e8de9770bd
Mateusz Sobierajski <mateusz.sobierajski.official@gmail.com>
2023-11-29 21:23:19 +0100

    Test 1

--- RPICommunication/ArduinoCOM/COMListener.py
+++ RPICommunication/ArduinoCOM/COMListener.py
@@ -13,8 +13,8 @@ def listener():
 			splitinfo = line.split(",")
 			print(line)
 			print(splitinfo)
-			print(splitinfo[1])
-			print(splitinfo[3])
+			#print(splitinfo[1])
+			#print(splitinfo[3])
 			for a in splitinfo:
 				print(a)
 
