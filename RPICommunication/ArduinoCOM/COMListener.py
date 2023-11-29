46bff36604dad97dc90b0e83d5682f45f973107b
Mateusz Sobierajski <mateusz.sobierajski.official@gmail.com>
2023-11-29 11:52:08 +0100

    Small fixes 5

--- RPICommunication/ArduinoCOM/COMListener.py
+++ RPICommunication/ArduinoCOM/COMListener.py
@@ -4,14 +4,20 @@ import time
 
 # ser = serial.Serial('/dev/ttyACM0',9600)
 def listener():
-	ser = serial.Serial('/dev/ttyACM0', 1200)
-	s = [0, 1]
+	ser = serial.Serial('/dev/ttyUSB0', 1200)
+	ser.reset_input_buffer()
 	while True:
-		read_serial = ser.readline()
-		print(ser.readline())
-		s[0] = str(int(ser.readline(), 16))
-		print(s[0])
-		print(read_serial)
+		if ser.inWaiting() > 0:
+			print(ser.readline())
+			line = ser.readline().decode('utf-8').rstrip()
+			print(line)
+
+	#while True:
+	#	read_serial = ser.readline()
+	#	print(ser.readline())
+		#s[0] = str(int(ser.readline(), 16))
+		#print(s[0])
+	#	print(read_serial)
 
 
 def listener2():
