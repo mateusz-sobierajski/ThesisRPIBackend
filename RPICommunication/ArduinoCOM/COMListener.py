dde5f8ed848809f645a845bbc529328c70adb743
Mateusz Sobierajski <mateusz.sobierajski.official@gmail.com>
2023-12-07 23:12:14 +0100

    Database test 15

--- RPICommunication/ArduinoCOM/COMListener.py
+++ RPICommunication/ArduinoCOM/COMListener.py
@@ -14,22 +14,22 @@ def listener():
 			ii += 1
 			line = ser.readline().decode('utf-8').rstrip()
 			splitinfo = line.split(",")
-			print(line)
-			print(splitinfo)
+			#print(line)
+			#print(splitinfo)
 			#print(splitinfo[1])
 			#print(splitinfo[3])
 			#for a in splitinfo:
 			#	print(a)
-			print ("Init ii counter: ")
-			print (ii)
+			#print ("Init ii counter: ")
+			#print (ii)
 			if ii > 4:
 				#insert_aht10(splitinfo)
 				print(splitinfo[1])
-			for i in range(len(splitinfo)):
-				print(i)
-				type(i)
-				print(splitinfo[i])
-				type(splitinfo[i])
+			#for i in range(len(splitinfo)):
+			#	print(i)
+			#	type(i)
+			#	print(splitinfo[i])
+			#	type(splitinfo[i])
 			#get_temp()
 
 
