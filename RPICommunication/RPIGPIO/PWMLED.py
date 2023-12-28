95cee307fe0702954ae10543bb83a06f9d7aa791
Mateusz Sobierajski <mateusz.sobierajski.official@gmail.com>
2023-12-28 20:04:01 +0100

    PWM test 7

--- RPICommunication/RPIGPIO/PWMLED.py
+++ RPICommunication/RPIGPIO/PWMLED.py
@@ -9,19 +9,7 @@ def gpioLED(value):
     pi_pwm = GPIO.PWM(ledpin, 1000)  # create PWM instance with frequency
     pi_pwm.start(0)  # start PWM of required Duty Cycle
     pi_pwm.ChangeDutyCycle(value)
-    try:
-        while True:
-            for duty in range(0, 101, 1):
-                pi_pwm.ChangeDutyCycle(duty)  # provide duty cycle in the range 0-100
-                sleep(0.01)
-            sleep(0.5)
-
-            for duty in range(100, -1, -1):
-                pi_pwm.ChangeDutyCycle(duty)
-                sleep(0.01)
-            sleep(0.5)
-    except KeyboardInterrupt:
-        pass
+    sleep(0.5)
     pi_pwm.stop()
     GPIO.cleanup()
 
